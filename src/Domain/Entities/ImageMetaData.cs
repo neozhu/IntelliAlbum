7e3e8605700e174207b5e5948f1531153a64ba31
hualin.zhu <neo.js.cn@gmail.com>
2023-05-31 20:27:34 +0800

    add rescan

--- src/Domain/Entities/ImageMetaData.cs
+++ src/Domain/Entities/ImageMetaData.cs
@@ -12,6 +12,7 @@ public class ImageMetaData
     public DateTime DateTaken { get; set; }
     public int Width { get; set; }
     public int Height { get; set; }
+    public long FileSize { get; set; }
     public double AspectRatio { get; set; } = 1;
     public int Rating { get; set; } // 1-5, stars
     public string? Caption { get; set; }
@@ -51,13 +52,64 @@ public class ImageMetaData
     public virtual List<DirectoryBase>? EXIFData { get; set; }
 
     public string? MimeType { get; set; }
+    public string? FileType { get; set; }
     public string? Category { get; set; }
     public string? City { get; set; }
     public override string ToString()
     {
         return $"{MimeType} {Category} {Caption} {Description} {City} { Copyright} {ISO} {FNum } {Exposure} {Camera?.Model} {Lens?.Model} {DateTaken}";
     }
+    public string ToImageInformation()
+    {
+        return $"{FileType}, {Width} x {Height}, {GetBytesReadable(FileSize)}";
+    }
 
+    private string GetBytesReadable(long i)
+    {
+        // Get absolute value
+        long absolute_i = (i < 0 ? -i : i);
+        // Determine the suffix and readable value
+        string suffix;
+        double readable;
+        if (absolute_i >= 0x1000000000000000) // Exabyte
+        {
+            suffix = "EB";
+            readable = (i >> 50);
+        }
+        else if (absolute_i >= 0x4000000000000) // Petabyte
+        {
+            suffix = "PB";
+            readable = (i >> 40);
+        }
+        else if (absolute_i >= 0x10000000000) // Terabyte
+        {
+            suffix = "TB";
+            readable = (i >> 30);
+        }
+        else if (absolute_i >= 0x40000000) // Gigabyte
+        {
+            suffix = "GB";
+            readable = (i >> 20);
+        }
+        else if (absolute_i >= 0x100000) // Megabyte
+        {
+            suffix = "MB";
+            readable = (i >> 10);
+        }
+        else if (absolute_i >= 0x400) // Kilobyte
+        {
+            suffix = "KB";
+            readable = i;
+        }
+        else
+        {
+            return i.ToString("0 B"); // Byte
+        }
+        // Divide by 1024 to get fractional value
+        readable = (readable / 1024);
+        // Return formatted number with suffix
+        return readable.ToString("0 ") + suffix;
+    }
 }
 
     public class Camera
